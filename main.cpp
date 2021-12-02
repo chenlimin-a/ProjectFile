ed2d3f3a0c210fda657bb05cae3f5526dc8a8b68
promisingchen <2389627261@qq.com>
2021-12-03 01:19:14 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "qwe.hpp"
+
+using namespace std;
+int main() {
+    cout << "нд╪Ч" << endl;
+    asd();
+    return 0;
+}
---
+++ qwe.hpp
@@ -0,0 +1,4 @@
+using namespace std;
+void asd(){
+    cout << "asd" << endl;
+}
